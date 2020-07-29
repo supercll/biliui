6fb5d00864e0cf58744bdee40b51af970e9bf27d
lc <1033581609@qq.com>
2020-07-29 15:14:21 +0800

    初始化aside 的 html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -3,6 +3,23 @@
         <Topnav />
         <div class="content">
             <aside>边栏</aside>
+            <aside>
+                <h2>组件列表</h2>
+                <ol>
+                    <li>
+                        <router-link to="/doc/switch">Switch 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/button">Button 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/dialog">Dialog 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/tabs">Tabs 组件</router-link>
+                    </li>
+                </ol>
+            </aside>
             <main>主内容</main>
         </div>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
             <h1>咕噜UI</h1>
             <h2>一个简易的UI框架</h2>
             <p class="actions">
-                <a href>Github</a>
-                <a href>开始</a>
+                <a href="https://github.com">Github</a>
+                <router-link to="/document">开始</router-link>
             </p>
         </div>
     </div>
@@ -16,9 +16,30 @@
 import Topnav from "../components/Topnav.vue";
 export default {
     name: "Home",
-    components: {Topnav},
+    components: { Topnav },
 };
 </script>
 
 <style lang="scss" scoped>
+.banner {
+    padding: 100px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .actions {
+        padding: 8px 0;
+        a {
+            margin: 0 8px;
+            background: #fff;
+            display: inline-block;
+            $h: 28px;
+            height: $h;
+            line-height: $h;
+            border-radius: $h/2;
+            padding: 0 8px;
+        }
+    }
+}
 </style>
