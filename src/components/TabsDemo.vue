6e95b32d828d7ec537e47f6c1f181ad2779b55e6
lc <1033581609@qq.com>
2020-09-05 19:09:04 +0800

    修复样式+实现tabs组件

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,13 +1,25 @@
 <template>
-    <h1>TabsDemo 文档</h1>
-
+    <div>Tabs 示例</div>
+    <h1>示例1</h1>
+    <Tabs v-model:selected="x">
+        <Tab title="导航1">内容1</Tab>
+        <Tab title="导航2">内容2</Tab>
+    </Tabs>
 </template>
-
-<script lang="ts" scpoed>
+<script lang="ts">
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+import { ref } from "vue";
 export default {
-    name: "SwitchDemo",
+    components: {
+        Tabs,
+        Tab,
+    },
+    setup() {
+        const x = ref("导航2");
+        return {
+            x,
+        };
+    },
 };
 </script>
-
-<style>
-</style>
