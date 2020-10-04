72afafd101e8fa1a7176be928d8f1abbb8f649e3
lc <1033581609@qq.com>
2020-10-04 22:39:21 +0800

    实现源码动画效果+清除多余组件

---
+++ src/components/Demo.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="demo">
+        <h2>{{component.__sourceCodeTitle}}</h2>
+        <div class="demo-component">
+            <component :is="component" />
+        </div>
+        <div class="demo-actions">
+            <Button @click="toggle">查看代码</Button>
+        </div>
+        <div :class="['demo-code', {codeVisible}]" ref="codeRefs">
+            <pre
+                class="language-html"
+                v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')"
+            />
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+import { onMounted, ref } from "vue";
+import "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+const Prism = (window as any).Prism;
+export default {
+    props: {
+        component: Object,
+    },
+    setup() {
+        const codeRefs = ref(null);
+        let code, codeHight;
+        onMounted(() => {
+            code = codeRefs.value;
+            codeHight = code.offsetHeight;
+            code.style.height = `${codeHight}px`;
+        });
+        const toggle = () => {
+            if (code.style.height === "0px") {
+                code.style.height = `${codeHight}px`;
+            } else {
+                code.style.height = "0";
+            }
+        };
+        return {
+            Prism,
+            Button,
+            codeRefs,
+            toggle,
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+.demo {
+    border: 1px solid $border-color;
+    margin: 16px 0 32px;
+    > h2 {
+        font-size: 20px;
+        padding: 8px 16px;
+        border-bottom: 1px solid $border-color;
+    }
+    &-component {
+        padding: 16px;
+    }
+    &-actions {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+    }
+    &-code {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+        transition: all 0.3s ease;
+        position: relative;
+        overflow: hidden;
+        > pre {
+            line-height: 1.1;
+            font-family: Consolas, "Courier New", Courier, monospace;
+            margin: 0;
+        }
+    }
+}
+</style>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,77 +1,28 @@
 <template>
     <h1 class="doc-header">Switch 示例</h1>
-    <div class="demo">
-        <h2>普通开关</h2>
-        <div class="demo-component">
-            <component :is="Switch1Demo" />
-        </div>
-        <div class="demo-code">
-            <pre
-                class="language-html"
-                v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')"
-            />
-        </div>
-    </div>
-    <div class="demo">
-        <h2>文字开关</h2>
-        <div class="demo-component">
-            <component :is="Switch2Demo" />
-        </div>
-        <div class="demo-actions">
-            <Button>查看代码</Button>
-        </div>
-        <div class="demo-code">
-            <pre
-                class="language-html"
-                v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')"
-            />
-        </div>
-    </div>
-    <div class="demo">
-        <h2>blibili图标开关</h2>
-        <div class="demo-component">
-            <component :is="Switch3Demo" />
-        </div>
-        <div class="demo-code">
-            <pre
-                class="language-html"
-                v-html="Prism.highlight(Switch3Demo.__sourceCode, Prism.languages.html, 'html')"
-            />
-        </div>
-    </div>
+    <Demo :component="Switch1Demo" />
+    <Demo :component="Switch2Demo" />
+    <Demo :component="Switch3Demo" />
 </template>
 
 <script lang="ts">
-import Switch from "../lib/Switch.vue";
-import Button from "../lib/button.vue";
 import Switch1Demo from "../components/Switch1.demo.vue";
 import Switch2Demo from "../components/Switch2.demo.vue";
 import Switch3Demo from "../components/Switch3.demo.vue";
-import "prismjs";
-import "prismjs/themes/prism-tomorrow.css";
-const Prism = (window as any).Prism;
+import Demo from "../components/Demo.vue";
 import { ref } from "vue";
 export default {
     name: "SwitchDemo",
     components: {
-        Switch,
-        Button,
+        Demo,
     },
     setup() {
         return {
             Switch1Demo,
             Switch2Demo,
             Switch3Demo,
-            Prism,
         };
     },
 };
 </script>
 
-<style lang="scss">
-.demo {
-    div {
-        margin: 10px 0;
-    }
-}
-</style>
