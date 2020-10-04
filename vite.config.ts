a59c90709fc815e576cbc8679b7b57a20f8c22ba
lc <1033581609@qq.com>
2020-10-04 18:52:48 +0800

    merge冲突

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 // @ts-nocheck
-
 import { md } from "./plugins/md";
 import fs from "fs";
 import { baseParse } from "@vue/compiler-core";
-
 export default {
     plugins: [md()],
     vueCustomBlockTransforms: {
@@ -14,15 +12,9 @@ export default {
             const title = parsed.children[0].content;
             const main = file.split(parsed.loc.source).join("").trim();
             return `export default function (Component) {
-<<<<<<< HEAD
             Component.__sourceCode = ${JSON.stringify(main)}
             Component.__sourceCodeTitle = ${JSON.stringify(title)}
             }`.trim();
-=======
-        Component.__sourceCode = ${JSON.stringify(main)}
-        Component.__sourceCodeTitle = ${JSON.stringify(title)}
-      }`.trim();
->>>>>>> 78ed0b1a6f5bd4f59036d1838871cbee3a11a307
         },
     },
 };
