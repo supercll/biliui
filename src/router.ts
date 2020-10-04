eac9924a8e63d38c739dd58347febe19a4f8a857
lc <1033581609@qq.com>
2020-10-04 14:49:05 +0800

    通过路由控制组件

--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,20 @@
-import Home from "./views/Home.vue"    // ts只能引入ts文件
-import Doc from './views/Doc.vue'
-import Intro from './views/Intro.vue'
-import Install from './views/Install.vue'
-import Start from './views/Start.vue'
-import SwitchDemo from './components/SwitchDemo.vue'
-import ButtonDemo from './components/ButtonDemo.vue'
-import DialogDemo from './components/DialogDemo.vue'
-import TabsDemo from './components/TabsDemo.vue'
-import DocDemo from './components/DocDemo.vue'
+import Home from "./views/Home.vue"; // ts只能引入ts文件
+import Doc from "./views/Doc.vue";
+import Intro from "./views/Intro.vue";
+import MarkDown from "./components/MarkDown.vue";
+import Install from "./views/Install.vue";
+import Start from "./views/Start.vue";
+import SwitchDemo from "./components/SwitchDemo.vue";
+import ButtonDemo from "./components/ButtonDemo.vue";
+import DialogDemo from "./components/DialogDemo.vue";
+import TabsDemo from "./components/TabsDemo.vue";
+import DocDemo from "./components/DocDemo.vue";
 
-import { createWebHashHistory, createRouter } from 'vue-router';
+import { createWebHashHistory, createRouter } from "vue-router";
+import { h } from "vue";
 
 const history = createWebHashHistory();
+const x = path => h(MarkDown, { path, key: path });
 export const router = createRouter({
     history: history,
     linkExactActiveClass: "doc-active",
@@ -19,20 +22,29 @@ export const router = createRouter({
     routes: [
         { path: "/", component: Home },
         {
-            path: "/doc", component: Doc, children: [
+            path: "/doc",
+            component: Doc,
+            children: [
                 { path: "", component: DocDemo },
-                { path: "intro", component: Intro },
-                { path: "install", component: Install },
-                { path: "start", component: Start },
-                { path: 'switch', component: SwitchDemo },
-                { path: 'button', component: ButtonDemo },
-                { path: 'dialog', component: DialogDemo },
-                { path: 'tabs', component: TabsDemo },
-            ]
-        }
-    ]
+                {
+                    path: "intro",
+                    component: x("../markdown/intro.md"),
+                },
+                {
+                    path: "install",
+                    component: x("../markdown/install.md"),
+                },
+                {
+                    path: "start",
+                    component: x("../markdown/start.md"),
+                },
+                { path: "switch", component: SwitchDemo },
+                { path: "button", component: ButtonDemo },
+                { path: "dialog", component: DialogDemo },
+                { path: "tabs", component: TabsDemo },
+            ],
+        },
+    ],
 });
 
-router.afterEach(() => {
-
-})
+router.afterEach(() => {});
