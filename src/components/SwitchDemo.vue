1d0ed44598c89d90dfccd4a1af852ff16953aa62
lc <1033581609@qq.com>
2020-10-04 21:18:06 +0800

    超好看的源码高亮插件prismjs

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -3,31 +3,40 @@
     <div class="demo">
         <h2>普通开关</h2>
         <div class="demo-component">
-            <component :is="Swicth1Demo" />
+            <component :is="Switch1Demo" />
         </div>
         <div class="demo-code">
-            <pre>{{Switch1Demo.__sourceCode}}</pre>
+            <pre
+                class="language-html"
+                v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')"
+            />
         </div>
     </div>
     <div class="demo">
         <h2>文字开关</h2>
         <div class="demo-component">
-            <component :is="Swicth3Demo" />
+            <component :is="Switch2Demo" />
         </div>
         <div class="demo-actions">
             <Button>查看代码</Button>
         </div>
         <div class="demo-code">
-            <pre>{{Switch2Demo.__sourceCode}}</pre>
+            <pre
+                class="language-html"
+                v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')"
+            />
         </div>
     </div>
     <div class="demo">
-        <h2>图标开关</h2>
+        <h2>blibili图标开关</h2>
         <div class="demo-component">
-            <component :is="Swicth3Demo" />
+            <component :is="Switch3Demo" />
         </div>
         <div class="demo-code">
-            <pre>{{Switch3Demo.__sourceCode}}</pre>
+            <pre
+                class="language-html"
+                v-html="Prism.highlight(Switch3Demo.__sourceCode, Prism.languages.html, 'html')"
+            />
         </div>
     </div>
 </template>
@@ -38,6 +47,9 @@ import Button from "../lib/button.vue";
 import Switch1Demo from "../components/Switch1.demo.vue";
 import Switch2Demo from "../components/Switch2.demo.vue";
 import Switch3Demo from "../components/Switch3.demo.vue";
+import "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+const Prism = (window as any).Prism;
 import { ref } from "vue";
 export default {
     name: "SwitchDemo",
@@ -50,6 +62,7 @@ export default {
             Switch1Demo,
             Switch2Demo,
             Switch3Demo,
+            Prism,
         };
     },
 };
