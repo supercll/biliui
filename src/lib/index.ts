eb7852a5ecf8d646bbec94429534a6e1057aeb74
lc <1033581609@qq.com>
2020-10-05 10:18:16 +0800

    创建lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,10 @@
+/* import Switch from "./Switch.vue";
+export { Switch }; 
+// 将switch 默认的内容当做switch导出
+*/
+export { default as Switch } from "./Switch.vue";
+export { default as Button } from "./Button.vue";
+export { default as Tabs } from "./Tabs.vue";
+export { default as Tab } from "./Tab.vue";
+export { default as Dialog } from "./Dialog.vue";
+export { openDialog } from "./openDialog";
